adbc48974c429e4ec7ed5d7032514bebb2ea202f
Robin Gibbs <r.gibbs86@gmail.com>
2023-01-12 15:28:21 +0000

    update

--- Practise1.py
+++ Practise1.py
@@ -1,1 +1,23 @@
-print(2+3)
+print('how many cats own?')
+numCats = input()
+
+try:
+    if int(numCats) >= 4:
+        print('thats a lot!')
+    else:
+        print('not many!')
+
+except ValueError:
+    print('not a number!')
+
+
+
+eggs = 10
+
+def spam(count):
+        global eggs
+        eggs = count + eggs
+
+spam(eggs)
+
+print(eggs)
